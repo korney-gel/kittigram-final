a1a654d2681ba5629798547a58a07f7263aab3f5
Denis Kornienko <korney.turk@gmail.com>
2025-04-03 15:44:33 +0300

    Fix boot_disk params on ubuntu-2404-lts

--- infra/main.tf
+++ infra/main.tf
@@ -5,66 +5,86 @@ terraform {
     }
     bucket = "kittygram-terraform-state"
     region = "ru-central1"
-    key = "tf-state.tfstate"
-    skip_region_validation = true
+    key    = "tf-state.tfstate"
+    skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id = true
-    skip_s3_checksum = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "~> 0.89.0"
     }
   }
 }
 
 provider "yandex" {
-  zone = "ru-central1-a"
-  cloud_id = "b1gjhns5mdgfvvsk8qud"
+  zone      = "ru-central1-a"
+  cloud_id  = "b1gjhns5mdgfvvsk8qud"
   folder_id = "b1glgve5lotc5cunfnm5"
   service_account_key_file = "key.json"
 }
 
-# Создаём виртуальную сеть - изолированный сетевой домен для наших ресурсов
-# Сети в Yandex Cloud подобны VPC (Virtual Private Cloud) в других облачных провайдерах
 resource "yandex_vpc_network" "kittygram-network" {
-  name = "kittygram-network"                   # Человекочитаемое название ресурса
+  name = "kittygram-network"
 }
 
-# Создаём подсеть в рамках нашей виртуальной сети
-# Подсеть - это диапазон IP-адресов внутри сети, привязанный к конкретной зоне доступности
 resource "yandex_vpc_subnet" "kittygram-subnet" {
-  name           = "kittygram-subnet"          # Название подсети
-  zone           = "ru-central1-a"         # Зона доступности должна совпадать с провайдером
-  network_id     = yandex_vpc_network.kittygram-network.id  # Связь с родительской сетью
-  v4_cidr_blocks = ["10.2.0.0/16"]         # Диапазон IP-адресов в нотации CIDR (65536 адресов)
+  name           = "kittygram-subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.kittygram-network.id
+  v4_cidr_blocks = ["10.2.0.0/16"]
 }
 
+resource "yandex_vpc_security_group" "kittygram-sg" {
+  name       = "kittygram-security-group"
+  network_id = yandex_vpc_network.kittygram-network.id
 
+  egress {
+    protocol       = "ANY"
+    description    = "Allow all outgoing traffic"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Allow SSH"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Allow HTTP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
+  }
+}
+
+data "yandex_compute_image" "Ubuntu" {
+  family    = "Ubuntu-20-04-lts"
+  folder_id = "standard-images"
+}
 
 resource "yandex_compute_instance" "vm" {
-  name = "kittygram-terraform-vm" # Имя ВМ
-
+  name = "kittygram-terraform-vm"
   resources {
     cores  = 2
     memory = 2
   }
-
- boot_disk {
+  boot_disk {
     initialize_params {
-      image_id = "f2e3s8u6ecl26615m3pv"
+      image_id = data.yandex_compute_image.Ubuntu.id
     }
   }
-
-
   network_interface {
-    subnet_id = yandex_vpc_subnet.kittygram-subnet.id
-    nat       = true
+    subnet_id          = yandex_vpc_subnet.kittygram-subnet.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.kittygram-sg.id]
   }
-
   metadata = {
-    ssh-keys = "ubuntu:${var.ssh_public_key}"
+    ssh-keys = "Ubuntu:${var.ssh_public_key}"
   }
 }
 
@@ -78,5 +98,3 @@ variable "yc_service_account_key" {
   type        = string
   sensitive   = true
 }
-
-
